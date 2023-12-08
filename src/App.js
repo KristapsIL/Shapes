27d872925b083e78a98f100e13a1162d60eedee8
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-12-08 11:37:10 +0200

    Pog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect} from "react";
 function App() {
+  useEffect(()=>{
+    async function getData(){
+      const response = await fetch("http://localhost:8080/");
+      const data = await response.json();
+      console.log(data)
+    }
+    getData();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
